8723ccda9cca359308c63cefa4afbab08864b428
Phil Watts <phil@hashicorp.com>
2016-12-14 09:32:32 -0500

    added self-describe-tags iam instance profile

--- main.tf
+++ main.tf
@@ -29,6 +29,41 @@ data "aws_iam_policy_document" "flowlog-assumerole" {
   }
 }
 
+data "aws_iam_policy_document" "ec2_self_describe" {
+  statement {
+    actions = [
+      "ec2:DescribeTags"
+    ]
+    resources = [
+      "arn:aws:ec2:*"
+    ]
+    effect = "Allow"
+  }
+}
+
+data "template_file" "ec2_assume_role" {
+  template = "${file("templates/assume_role_policy.json")}"
+  vars {
+    aws_service = "ec2.amazonaws.com"
+  }
+}
+
+resource "aws_iam_role" "self_tags" {
+  name = "describe-tags-self"
+  assume_role_policy = "${data.template_file.ec2_assume_role.rendered}"
+}
+
+resource "aws_iam_role_policy" "self_tags" {
+  name = "describe-tags-self"
+  role = "${aws_iam_role.self_tags.id}"
+  policy = "${data.aws_iam_policy_document.ec2_self_describe.json}"
+}
+
+resource "aws_iam_instance_profile" "self_tags" {
+  name = "describe-tags-self"
+  roles = ["${aws_iam_role.self_tags.name}"]
+}
+
 resource "aws_iam_role" "flowlog" {
   name = "flowlog"
   assume_role_policy = "${data.aws_iam_policy_document.flowlog-assumerole.json}"
@@ -88,3 +123,4 @@ data "aws_iam_policy_document" "cloudtrail" {
 }
 
 output "flowlogrole" { value = "${aws_iam_role.flowlog.arn}" }
+output "describe_tags_self" { value ="${aws_iam_instance_profile.self_tags.id}" }
